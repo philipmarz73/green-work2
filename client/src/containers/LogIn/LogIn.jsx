ad97f9c349cd8cad6ca7ed2b06936c1f3a92c4ec
Phlipper <marzullop73@gmail.com>
2021-02-19 01:05:21 -0500

    adding fields to Login page

--- client/src/containers/LogIn/LogIn.jsx
+++ client/src/containers/LogIn/LogIn.jsx
@@ -8,8 +8,28 @@ const LogIn = () => {
         <h1 className="center-align">Log In</h1>
             </div> 
             </div>
+            <div className="row">
+            <form className="col s12" 
+              
+              <label htmlfor="title">Email</label>
+              <div className="row">
+
+
+                <div className="input-field col s6">
+                  <input placeholder="Plant Name" 
+                  id="title" 
+                  type="text" 
+                  />
+        <label htmlfor="title">Password</label>
         </div>
+      </div>
+      </form>
+      </div>
+      </div>
+                 
     );
 };
 
 export default LogIn;
+
+{/* onSubmit={handleFormSubmit}></div> */}
