b839580f6b1032e1d3d405105aae7eef57ad1dbd
Phlipper <marzullop73@gmail.com>
2021-02-18 22:51:59 -0500

    moved objects and methods over from MyGarden to GardenTableRow to create Table component

--- client/src/components/GardenTableRow/GardenTableRow.jsx
+++ client/src/components/GardenTableRow/GardenTableRow.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
-const GardenTableRow = props => {
+const GardenTableRow = ({
+    _id,
+    title,
+    imageURL,
+    plantType,
+    scientificName,
+    moistureTolerance,
+    sunTolerance,
+    maxHeight,
+    getPlants,
+}) => {
+    const deletePlant = (id) => {
+        console.log("delete a plant");
+        console.log("id");
+        axios
+        .delete(`/api/plants/${id}`)
+        .then(() => {
+          getPlants();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      
+      };
     return (
-        <div>
+        <tr>
+            <td>{title}</td>
+            <td><img src={imageURL} alt={title} style={{height: "4em"}}></img>
+            </td>
+            <td>{plantType}</td>
+            <td>{scientificName}</td>
+            <td>{moistureTolerance}</td>
+            <td>{sunTolerance}</td>
+            <td>{maxHeight}</td>
+            <td>
+              <FontAwesomeIcon icon={faTrash} 
+              onClick={() => {deletePlant(_id);
+              }}
+              />
+              </td>
+
+
+
+
+
+
+
+
+
+
+
+
+        </tr>
+        
             
-        </div>
+       
     );
 };
 
